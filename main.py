1b7b8c53bc55c92f030db718a2a510453296fc7d
Muhammad Syahril Syarif <103059057+syahrilz@users.noreply.github.com>
2024-06-09 02:06:54 +0700

    Update and rename script.js to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from dotenv import load_dotenv
+import time
+from pypresence import Presence
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'I have been logged in as {client.user}')
+    
+    RPC = Presence("1017617990331666442")  # Client ID
+    RPC.connect()
+    
+    RPC.update(
+        state="Inactive",
+        details="</>",
+        large_image="off",
+        start=time.time(),
+        buttons=[{"label": "Watch Stream", "url": "https://www.twitch.tv/syahrills"}]
+    )
+
+client.run(TOKEN)
